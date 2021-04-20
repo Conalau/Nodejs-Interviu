0e18c368e3e457059eb2bb35cbfb627e898c8cd7
Coni <constantinescu.andrei03@gmail.com>
2021-04-20 11:38:13 +0300

    List doctors from csv

--- src/doctors/docs.router.ts
+++ src/doctors/docs.router.ts
@@ -2,14 +2,20 @@ import express, {Request, Response, text}  from "express";
 import * as DocService from "./docs.service";
 import {BaseDoc, Doc} from "./doc.interface"
 import { count } from "node:console";
+import fs from "fs";
+import parse from "csv-parser";
 
 export const docsRouter = express.Router();
 
 let arr: number[] = [];
 
+
+
+const csvFile = "./nodejs.csv";
+
 function countInArray(array: number[], nr: number) {
-    var count = 0;
-    for (var i = 0; i < array.length; i++) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
         if (array[i] === nr) {
             count++;
         }
@@ -39,3 +45,30 @@ docsRouter.post("/add-doctor", async (req: Request, res: Response)=> {
         res.status(500).send(e.message);
     }
 });
+
+docsRouter.post("/doctors", async(req: Request, res:Response) => {
+        let arrDoctors : any[] = [];
+        fs.createReadStream(csvFile).on('error', () => {
+            throw new Error('Error');
+        })
+        .pipe(parse())
+        .on('data', (row) => {
+            if(arrDoctors.filter(e => e.ID == row.ID).length > 0){
+                if(arrDoctors.filter(e => e.FamilyName == row.FamilyName).length > 0 && arrDoctors.filter(e => e.GivenName == row.GivenName).length > 0){
+                    if(arrDoctors.filter(e => e.Active == true)) {
+                       let doc = arrDoctors.find(elem => elem.ID == row.ID);
+                        if(doc.NameId.toLowerCase().search(row.NameId.toLowerCase()) == -1){
+                            doc.NameId += ' ,' + row.NameId;
+                        } 
+                    }
+                }  else {
+                    throw new Error("Error! A doctor have an id that is already assigned")
+                }
+            } else {
+                arrDoctors.push(row);
+            }
+        })
+        .on('end', () => {
+            console.log(arrDoctors);
+        })
+})
